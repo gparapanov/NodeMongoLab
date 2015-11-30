5bc2a68d46da8909afcdba1298d427e9fb0ba214
Georgi <georgi.parapanov@gmail.com>
2015-11-30 15:10:37 +0000

    lab

--- twittertest.js
+++ twittertest.js
@@ -15,6 +15,10 @@ http.createServer(function(request,response){
     response.writeHead(200, { 'Content-Type': 'application/json','Access-Control-Allow-Origin':'*'});
 
     client.get('search/tweets',{q:'lolcat'},function(error,tweets){
-        response.end(JSON.stringify(tweets));
+        var json=[];
+        for(var i=0;i<tweets.statuses.length;i++){
+            json.push({name:tweets.statuses[i].user.name,text:tweets.statuses[i].text});
+        }
+        response.end(JSON.stringify(json));
     });
 }).listen(port);
